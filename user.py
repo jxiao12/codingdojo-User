7107afe5f312e713f56868f0cd439a37a298cf8d
j5xiao <81590932+j5xiao@users.noreply.github.com>
2022-01-18 16:57:31 -0800

    user

--- user.py
+++ user.py
@@ -5,12 +5,17 @@ class User:
         self.account_balance = 0
     # adding the deposit method
     def make_deposit(self, amount):	# takes an argument that is the amount of the deposit
-    	self.account_balance += amount	# the specific user's account increases by the amount of the value received
+    	self.account_balance += amount
+        return self	# the specific user's account increases by the amount of the value received
 
     def make_withdrawal(self, amount):
         self.account_balance -= amount
+        return self
+
     def display_user_balance(self): #"User: Guido van Rossum, Balance: $150
         return "User: " + self.name + ", Balance: $" + str(self.account_balance)
+
     def transfer_money(self, other_user, amount):
         self.account_balance -= amount
         other_user.make_deposit(amount)
+        return self
